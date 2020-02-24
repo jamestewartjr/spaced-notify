b3c8b56c71ae9774995c9797d9703f9b9ce121c9
James Stewart <jamestewartjr1@gmail.com>
2020-02-24 11:34:25 -0500

    Remove react styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Header} from './components/layout/Header'
 import {Content} from './components/layout/Content'
-import './App.css';
 
 function App() {
   return (
