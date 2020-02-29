5541bb4e0a65c8ae3b4273e48ec7c88e2883cb8d
James Stewart <jamestewartjr1@gmail.com>
2020-02-28 23:20:44 -0500

    wip for Projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,3 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useSelectedProjectValue, useProjectsValue } from '../context';
 
-export const 
+export const Projects = ({ activeValue = true}) => {
+  const [active, setActive] = useState(activeValue);
+  const [setSelectedProject] = useSelectedProjectValue();
+  const [projects] = useProjectsValue();
+
+  return (
+    projects && projects.map(project => (
+      <li 
+        key={project.projectId}
+        data-doc-id={project.docId}
+        data-testid="porject-action"
+        className={ active === project.projectId
+          ? 'active_sidebar__project'
+          : 'sidebar__project'
+        }
+        onClick={() => {
+          setActive(project.projectId);
+          setSelectedProject(project.projectId);
+        }}
+      >
+        I am a project
+      </li>
+    ))
+  )
+}
